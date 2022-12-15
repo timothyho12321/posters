673abafb975bf1c24f3fec15277d0e1b7a5cf87f
timothyho12321 <timhotim2@gmail.com>
2022-12-15 10:36:52 +0000

    done crud for 1tom; now migration for mtom; have not db up

--- routes/titles.js
+++ routes/titles.js
@@ -8,6 +8,7 @@ const { Title, MediaProperty } = require('../models')
 router.get('/', async (req, res) => {
     // #2 - fetch all the products (ie, SELECT * from products)
     let titles = await Title.collection().fetch();
+    console.log(titles);
     res.render('titles/index', {
         'titles': titles.toJSON() // #3 - convert collection to JSON
     })
@@ -18,7 +19,7 @@ router.get('/add', async (req, res) => {
     const allMediaProperties = await MediaProperty.fetchAll().map((property) => {
         return [property.get("id"), property.get("name")]
     })
-    
+
 
     const form = createTitleForm(allMediaProperties);
     res.render('titles/create', {
@@ -34,11 +35,10 @@ router.post('/add', async function (req, res) {
 
 
     // CONTINUE FROM HERE
-const allMediaProperties = await MediaProperty.fetchAll().map((property) => {
-    return [property.get("id"), property.get("name")]
-})
-console.log("Test print media properties")
-console.log(allMediaProperties)
+    const allMediaProperties = await MediaProperty.fetchAll().map((property) => {
+        return [property.get("id"), property.get("name")]
+    })
+
 
 
     const titleForm = createTitleForm(allMediaProperties);
@@ -52,6 +52,7 @@ console.log(allMediaProperties)
             titleObject.set('stock', form.data.stock);
             titleObject.set('height', form.data.height);
             titleObject.set('width', form.data.width);
+            titleObject.set('media_property_id', form.data.media_property_id);
             await titleObject.save();
             res.redirect('/titles')
         },
@@ -84,8 +85,12 @@ router.get("/update/:poster_id", async function (req, res) {
         require: true
     })
 
-    const titleForm = createTitleForm()
-    
+    const allMediaProperties = await MediaProperty.fetchAll().map((property) => {
+        return [property.get("id"), property.get("name")]
+    })
+
+    const titleForm = createTitleForm(allMediaProperties)
+
     titleForm.fields.title.value = title.get('title');
     titleForm.fields.cost.value = title.get('cost');
     titleForm.fields.description.value = title.get('description');
@@ -93,6 +98,9 @@ router.get("/update/:poster_id", async function (req, res) {
     titleForm.fields.stock.value = title.get('stock');
     titleForm.fields.height.value = title.get('height');
     titleForm.fields.width.value = title.get('width');
+    
+
+    titleForm.fields.media_property_id.value = title.get('media_property_id');
 
     res.render('titles/update', {
         'form': titleForm.toHTML(bootstrapField)
@@ -102,8 +110,12 @@ router.get("/update/:poster_id", async function (req, res) {
 })
 
 
-router.post('/update/:poster_id', async function(req,res){
+router.post('/update/:poster_id', async function (req, res) {
     const posterId = req.params.poster_id;
+
+    const allMediaProperties = await MediaProperty.fetchAll().map((property) => {
+        return [property.get("id"), property.get("name")]
+    })
 
     const title = await Title.where({
         'id': posterId
@@ -111,12 +123,12 @@ router.post('/update/:poster_id', async function(req,res){
         require: true
     })
 
-    const titleForm = createTitleForm()
+    const titleForm = createTitleForm(allMediaProperties)
 
     // pass the request as the first parameter
     // second parameter: object that has three handlers
     titleForm.handle(req, {
-        'success': async function(form) {
+        'success': async function (form) {
             // form has no validation error
             // then we proceed to update the product
             // Instead of:
@@ -128,8 +140,7 @@ router.post('/update/:poster_id', async function(req,res){
 
             // extract out the tags, but all the other keys in form.data
             // will go into productData
-           
-            
+
             title.set('title', form.data.title);
             title.set('cost', form.data.cost);
             title.set('description', form.data.description);
@@ -137,33 +148,64 @@ router.post('/update/:poster_id', async function(req,res){
             title.set('stock', form.data.stock);
             title.set('height', form.data.height);
             title.set('width', form.data.width);
-
+            title.set('media_property_id', form.data.media_property_id)
             title.save(); // make the change permanent
 
             res.redirect('/titles');
-          
+
         },
-        // HOW COME CANNOT SHOW ERROR MESSAGE 
-        'empty': async function(form) {
+       
+        'empty': async function (form) {
             // form has no data
-           
-            res.render('titles/update',{
-                
+
+            res.render('titles/update', {
+
                 'form': form.toHTML(bootstrapField)
             })
         },
-        'error': async function(form) {
+        'error': async function (form) {
             // one or more fields have validation errors
-            
+
             res.render('titles/update', {
                 'form': form.toHTML(bootstrapField)
-                
+
             })
         }
     })
 
 })
 
+
+router.get('/delete/:poster_id', async function(req,res){
+    // get the product that we want to delete
+    const title = await Title.where({
+        'id': req.params.poster_id
+    }).fetch({
+        require: true // means if there is no results, Bookshelf will cause an exception (i.e an error)
+    });
+
+    // console.log(title);
+    res.render('titles/delete',{
+        'title': title.toJSON()
+    })
+})
+
+
+router.post('/delete/:poster_id', async function(req,res){
+    // retrieving the object that represents the row
+    // which we want to delete
+    const title = await Title.where({
+        'id': req.params.poster_id
+    }).fetch({
+        require: true // means if there is no results, Bookshelf will cause an exception (i.e an error)
+    });
+
+    // execute the delete
+    await title.destroy();
+
+    res.redirect('/titles')
+
+})
 
 
 
